92f722d3cb7a0565cd2c1c588401ab42253b31ee
gparent <370905+gparent@users.noreply.github.com>
2023-10-12 17:01:59 -0400

    Make WorldConquest a derived class

--- src/pyfoxhole/api.py
+++ src/pyfoxhole/api.py
@@ -8,10 +8,26 @@ LIVE_API_URL = "https://war-service-live.foxholeservices.com/api"
 LIVE2_API_URL = "https://war-service-live-2.foxholeservices.com/api"
 
 
-class WorldConquestApi:
-    def __init__(self, api_url: str | None, timeout: int | None = 15):
+class Api:
+    def __init__(
+        self, api_url: str | None, api_endpoint: str, timeout: int | None = 15
+    ):
         self.api_base_url = LIVE_API_URL if api_url is None else api_url
+        self.api_endpoint = api_endpoint
         self.timeout = timeout
+
+    def _api_request(self, endpoint: str):
+        url = f"{self.api_base_url}/{self.api_endpoint}/{endpoint}"
+        response_json = get(url, timeout=self.timeout).json()
+        return response_json
+
+
+class WorldConquestApi(Api):
+    def __init__(self, api_url: str | None, timeout: int | None = 15):
+        self.api_endpoint = "worldconquest"
+        super().__init__(
+            api_url=api_url, api_endpoint=self.api_endpoint, timeout=timeout
+        )
 
     def maps(self) -> list[str]:
         return self._api_request("maps")
@@ -30,8 +46,3 @@ class WorldConquestApi:
     def war_report(self, map_name: str) -> WarReportResponse:
         endpoint = f"warReport/{map_name}"
         return self._api_request(endpoint)
-
-    def _api_request(self, endpoint: str):
-        url = self.api_base_url + "/worldconquest/" + endpoint
-        response_json = get(url, timeout=self.timeout).json()
-        return response_json
