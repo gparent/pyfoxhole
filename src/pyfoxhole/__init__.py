ca0bfc4900628959775e3a57499a1e8a598c890f
gparent <370905+gparent@users.noreply.github.com>
2024-03-08 23:15:40 -0500

    Add shortcut imports

---
+++ src/pyfoxhole/__init__.py
@@ -0,0 +1,26 @@
+from .api import LIVE2_API_URL, LIVE3_API_URL, LIVE_API_URL, WorldConquestApi
+from .models import (
+    BaseFlags,
+    Faction,
+    MapDataResponse,
+    MapIcon,
+    MapItem,
+    MapTextItem,
+    WarReportResponse,
+    WarResponse,
+)
+
+__all__ = [
+    "BaseFlags",
+    "Faction",
+    "LIVE_API_URL",
+    "LIVE2_API_URL",
+    "LIVE3_API_URL",
+    "MapDataResponse",
+    "MapIcon",
+    "MapItem",
+    "MapTextItem",
+    "WarResponse",
+    "WarReportResponse",
+    "WorldConquestApi",
+]
